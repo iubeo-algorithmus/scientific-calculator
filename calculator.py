b5b6609071301b6e615ae0d6250970d1b70824e9
iubeo-algorithmus <rafaelpetiz.ds@gmail.com>
2026-01-26 11:38:39 -0300

    feat: add value-rounding functions
    fix: shorten "power" function

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
 class ScientificCalculator:
+    # All of these will be removed
+
     def __init__(self, current_number):
         self.last_result = current_number
 
@@ -22,27 +24,36 @@ class ScientificCalculator:
         self.last_result = n1 // n2
         return self.last_result
 
+    # Could be done better
+    def power(self, num, power):
+        self.last_result = num ** power
+        return self.last_result
+
     # Placeholder until I can find a better way
     def square_root(self, num):
+        self.last_result = num
+
         for multiple in range(num // 2):
             if multiple * multiple == num:
                 self.last_result = multiple
                 return self.last_result
-        return num
+
+        return self.last_result
 
     def variable_root(self, num, root):
         pass
 
-    # Could be done better
-    def power(self, num, power):
-        result = 1
+    # Absolute value
+    def absolute_of(self, num):
+        pass
 
-        for i in range(power):
-            result = result * num
-
-        self.last_result = result
+    # Rounding values
+    def ceiling_of(self, num):
+        self.last_result = (num+1) // 1
         return self.last_result
 
-    # Absolute value
+    def floor_of(self, num):
+        self.last_result = num // 1
+        return self.last_result
 
     # Trigonometrics
