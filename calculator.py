4940cd21e590c583f8d2154e4c9699454c04a4d6
iubeo-algorithmus <rafaelpetiz.ds@gmail.com>
2026-01-27 16:08:41 -0300

    feat: add get_pi
    chore: add comments to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 class ScientificCalculator:
-    # All of these will be removed
+    # TODO: move last_result to a .txt to get a list of calculation results
 
     def __init__(self, input_number):
         self.last_result = input_number
@@ -24,7 +24,7 @@ class ScientificCalculator:
         self.last_result = first_input / second_input
         return self.last_result
 
-    # Could be done better
+    # Concise, but I want to do it by-hand in the future
     def power(self, input_number, power):
         self.last_result = input_number ** power
         return self.last_result
@@ -33,6 +33,7 @@ class ScientificCalculator:
     def square_root(self, input_number):
         self.last_result = input_number
 
+        # Find square root by iterating up to half of the input number
         for multiple in range(input_number // 2):
             if multiple * multiple == input_number:
                 self.last_result = multiple
@@ -45,23 +46,42 @@ class ScientificCalculator:
 
     # Absolute value
     def absolute_of(self, input_number):
-        pass
+    #   Another way:
+    #   return abs(input_number)
 
-    # Rounding values
-    def ceiling_of(self, input_number):
-        self.last_result = (input_number+1) // 1
+        if input_number < 0:
+            # Invert to positive if the value is negative
+            self.last_result = input_number * -1
+        else:
+            # If not negative, receive the value as-is
+            self.last_result = input_number
+
         return self.last_result
 
+    # Floor-dividing by 1 to return it rounded down
     def floor_of(self, input_number):
         self.last_result = input_number // 1
         return self.last_result
 
+    # Same as above, but adding 1 to the floor-divided value to return it "rounded up"
+    def ceiling_of(self, input_number):
+        self.last_result = (input_number+1) // 1
+        return self.last_result
+
     # Trigonomics
     def sine(self, input_number):
+        # sin0: opposite / hypotenuse
         pass
 
     def cosine(self, input_number):
+        # cos0: adjacent / hypotenuse
         pass
 
     def tangent(self, input_number):
+        # tan0: opposite / adjacent
         pass
+
+    # Others
+    def get_pi(self):
+        self.last_result = 3.14
+        return self.last_result
